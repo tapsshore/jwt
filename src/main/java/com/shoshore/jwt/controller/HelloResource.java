c4ec119b51ddf34e7ab303010e62829c585c2c54
Tapiwanashe Shoshore <tapiwanashe.shoshore@wonderlabz.com>
2022-10-21 17:10:10 +0200

    #01 - HelloResource

---
+++ src/main/java/com/shoshore/jwt/controller/HelloResource.java
@@ -0,0 +1,12 @@
+package com.shoshore.jwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloResource {
+@RequestMapping({"/hello"})
+  public String hello(){
+  return "Hello World";
+}
+}
